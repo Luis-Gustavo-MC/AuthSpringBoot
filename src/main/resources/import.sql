6d17c8dcaba37d50fb5bee940728139772ec9ebe
Luis-Gustavo-MC <lgmc120@gmail.com>
2026-04-21 22:41:13 -0300

    BCript password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$p0/J2p50.jMQdupkfVkKgOwdkvpJDV42RVdTfWwDHG84F5TvFm8Pe');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$p0/J2p50.jMQdupkfVkKgOwdkvpJDV42RVdTfWwDHG84F5TvFm8Pe');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
